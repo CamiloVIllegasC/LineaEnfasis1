ceb17f40f7cc4a2699fca6b70da4f5aca755c29e
CamiloVillegasC <dvillegasc04@gmail.com>
2026-02-10 19:30:25 -0500

    se modifica la importacion de dotenv para corregir llamado a la db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import express from "express";
 import logger from "morgan";
-import dotenv from "dotenv";
 import router from "./routes/citas.routes.js";
 
 dotenv.config();
